221f23471dcf1e0c1ddc65c084c956e1a0007dda
Indradip <chadsigmarizzler@gmail.com>
2025-01-25 21:09:29 +0600

    first commit

---
+++ average.py
@@ -0,0 +1,10 @@
+nooria = 1780
+indradip = 1945
+nahian = 60
+
+total = nooria + indradip + nahian
+print(f"Total Bill : {total}")
+
+avg = total/3
+
+print(f"Each person should pay : {avg}")
---
+++ countthenotes.py
@@ -0,0 +1,11 @@
+amount =int (input("Please Amount for Withdraw : "))
+
+note100 = amount//100
+
+note50 = (amount%100)//50
+
+note10 = ((amount%100)%50)//10
+
+print(f"notes of 100 taka {note100}")
+print(f"notes of 50 taka {note50}")
+print(f"notes of 10 taka {note10}")
